52b1e161897995914345cc11af4f270ad63fce08
Stéphane ZE-OGIER <contact@ze-ogier.com>
2020-08-10 14:04:20 +0200

    challenge2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # get 'answer/display'
+  get 'ask', to: 'questions#ask'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'answer', to: 'questions#answer'
 end
